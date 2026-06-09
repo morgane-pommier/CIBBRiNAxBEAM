0507eac4d9aec207fcd344fbaaa2dffc56aec122
Morgane Pommier <67705024+morgane-pommier@users.noreply.github.com>
2026-06-09 11:51:00 +0100

    Update reliability_estimation_CIBBRiNA.R
    Made updates for use in CIBBRiNA:
    - Works on the output of calc_total() using similar inputs
    - Integrates both the RMSE comparison and the CI breadth < 2 check
    - Returns a table with individual and combined checks outcomes (TRUE/FALSE)
    
    Outstanding questions regarding the comparison and averaging of RMSE in predictions.

--- reliability_estimation_CIBBRiNA.R
+++ reliability_estimation_CIBBRiNA.R
@@ -1,217 +1,178 @@
 ## ---------------------------
-##
-## Script name: RMSE_bpues_V5.R
-##
-## Purpose of script: Calculate root mean square error (RMSE) between BPUE estimates
-##                    and leave-one-out jacknife estimates, to help assess the
-##                    uncertainty of any given BPUE estimate
-##
-## Authors: ToR C members
-##
+## Authors :  WGBYC ToR C members. Original scripts available at https://github.com/dlusseau/BEAM/tree/main
+## Adapted for CIBBRiNA users by MP.
 ## ---------------------------
+#' 
+#' @description
+#' `reliability_estimation` Runs on the output of `calc_total()`. Fits a full glmmTMB model and leave-one-out jackknife models for each unique bycatch estimate. Computes RMSE between BPUE predictions from both models on the log scale. Returns the input data (total estimates) enriched with uncertainty and reliability metrics combining 1) model stability across jackknife folds at the BPUE level with 2) the breadth of the confidence interval around the total bycatch estimate. The bycatch estimate can be considered reliable is both checks are passed (overall_reliability).
+#' 
+#' @param tot data.table output from the total bycatch estimation function `calc_total()`, with values for for `analysis_resolution`, used to subset observations from `dat`. Also contains a column for `model` and `tot_mean`, the bycatch estimate value, if any.
+#' #' @param dat data.table with observed (monitored) fishing effort data. Same `dat` input table used in `calc_bpue()` and `calc_total()`
+#' @param response Character string. Name of the response variable. Typically, a column containing the number of individuals or bycatch incidents (positive integer values).
+#' @param effort_term Character string. Name of the variable to be used as an offset (e.g. number of days at sea, number of hooks, number of nets …), in `dat`. By default, the log of this covariate will be used as offset in the model. 
+#' @param analysis_resolution vector of column names, whose unique combinations are used to split the data given in `dat`. Must be the same vector used in `calc_bpue()` and `calc_total()` 
+#' @param include.weights boolean indicator indicating whether observations from the five most recent years should have double weight as compared to older data. Must match the value used in `calc_bpue`. 
+#' @param weight_values Optional. Numeric vector of observation weights. Must be of the same length that dat. Only used if include.weights = TRUE. Default is NULL.
+#' @returns A data.table identical to tot, with additional columns: 
+#' - rmse-val: root mean square error of jackknife vs full model predictions (log scale)
+#' - factor:  RMSE back-transformed from log scale 
+#' - lower_factor_prop — lower bound of the 68% uncertainty interval as a percentage: 100(1 - 1/factor)
+#' - upper_factor_prop — upper bound of the 68% uncertainty interval as a percentage: 100(factor - 1)
+#' - reliability_rmse —  TRUE/FASLE, whether both factor proportions are ≤ 25%
+#' - CI_breadth_<2 — TRUE/FASLE, whether the breadth of the confidence interval for the total bycatch estimate is smaller than 2 orders of magnitude.
+#' - Overall_reliability — TRUE/FASLE, whether both checks above are passed, or at least one is failed.
+#' @seealso [calc_bpue(), calc_total()]
+#' @export
 
-# 1) Mean BPUE + CV of estimates (emmeans)
-# 2) Using LOO (=jacknife) so using ggpredict/emmeans we obtain a BPUE for the full model
-#   and a BPUE for the jacknifed. Then, (BPUE_full-BPUE_jacknifed)^2 [we'll have 
-#   as many values as rows of data for that unique combination of ecoregionxmetierxsps].
-#   Then, sum all those rests and divide them by n-1 (cases) and make the square root
+#Difference with BEAM original code: BEAM reliability_estimation_p.r scripts reads data, calculates RMSE using a calc_rmse function, and then fills in new columns in the input data. Here, everything is happening in a single reliability_estimation.r function, and input data is supplied in the function call. The updated function also includes the breath of CI calculations and assessment.
 
-# Summary: fit full model + fit jacknifed models + ggpredict both + do (full_pred-jacknifed_pred)^2
-# 
-
-# FILTER WHEN BPUE !IS.NA
-
-# ONLY FOR THOSE THAT WE HAVE A BPUE ESTIMATE
-
-# pa filter, del bpue dataframe, cada fila ya tiene la combinacion unica de cada ecoxmetierxsps
-# despues recuperar del full dataframe 
-
-# CHANGELOG:
-# 2025-10-15 AM Refactored code to take full advantage of data.table performance
-# benefits and support parallelization.
-# TODO: move calc_rmse into separate file calc_rmse.R? What do we do with the
-# rest of the code in this script then?
-# TODO: look into unresponsive console issue that sometimes occurs after 
-# parallel execution
-
-library(glmmTMB)
-library(ggeffects)
-library(emmeans)
-library(data.table)
-
-# Load data ----
-
-# AM 15/10: changed from read.csv to fread, to avoid an extra step of coercing to data.table later
-full_data <- fread("data/obs3.csv")
-bpues_estimates <- fread("results/bpue_table_print.csv") #We need to change that and supply both bpue and tot.
-
-# Preparing datasets
-bpues_estimates$RMSE <- NA
-# Remove NAs in BPUE estimates & 0 in obs3 DaS
-bpues_estimates <- bpues_estimates[!is.na(BPUE), ] #CIBBRiNA edit: We will treat the zeros separately as reliable.
-
-# Reduce the size removing NAs in totalbycatch
-# AM 15/10: changed to use data.table syntax. We should probably avoid spaces in var names...
-# bpues_estimates <- bpues_estimates[!is.na(bpues_estimates$total.bycatch.2024), ]
-bpues_estimates <- bpues_estimates[!is.na(`total bycatch 2023`), ] #change to tot_mean probably
-
-#complete_bpues$RMSE <- 0
-full_data <- full_data[daysatsea > 0, ] #That's obs, need to change to effort term whatever that is
-full_data <- full_data[!is.na(daysatsea), ]
-full_data <- full_data[taxon_bycatch_monitor_ok == TRUE, ] #That's specific to WGBYC, not needed in CIBBRiNA, we assume users clean their data first.
-# Add log of monitoring (DaS)
-
-full_data[, logDAS := log(daysatsea)]
-#full_data$logDAS <- log(full_data$daysatsea)
-
-
-calc_rmse <- function(full_data, bpue) {
+reliability_estimation <- function(tot,
+                      dat,
+                      analysis_resolution,
+                      effort_term,
+                      include.weights = FALSE,
+                      weights_values  = NULL) {
   
-    # helper function (to avoid repeated code)
-    fit_and_predict <- function(form, data, levels = NULL) {
-        fit <- suppressMessages(glmmTMB(formula = form, offset = logDAS, data = data, family = nbinom2)) #Need to get the weight option in there no ?
-        re <- lme4::findbars(form) # random effects part of model formulation (if any)
-        re <- sapply(re, function(x) as.character(x[[3]]))
-        re.n <- length(re) # number of random effects
 
-        if (re.n == 0) {
-            pred <- as.data.table(emmeans(fit, ~1, type = "response", offset = log(1)))
-            pred$predicted <- pred$response
-        } else {
-            type_arg <- ifelse(packageVersion("ggeffects") >= "2.0.0", "random", "re")
-            pred <- ggpredict(model = fit, type =  type_arg, terms = re, verbose = FALSE)
-            pred <- as.data.table(pred)
-            
-            if (!"facet" %in% colnames(pred)) {
-                pred[, facet := "dummy"]
-            }
-            # if (!is.null(levels) & inherits(levels, "list")) {
-            #     for (i in 1:length(levels)) {
-            #         if (length(levels[[i]])) {
-            #             nm <- names(levels)[i]
-            #             pred[, (nm) := factor(get(nm), levels = levels[[i]])]
-            #         }
-            #     }
-            # }
-            pred <- pred[order(x, group, facet)]
-        }
-
-        list(fit = fit, re = re, re.n = re.n, pred = pred)
+#Parallel set-up, updated with CIBBRiNA functions arguments
+  
+  if (nrow(tot) > 1) {
+    
+    if (exists("BEAM_pb")) {
+      BEAM_pb$terminate()
+      rm("BEAM_pb", envir = .GlobalEnv)
+    }
+    BEAM_pb <<- progress_bar$new(
+      format = "calc_rmse :percent :current/:total [:bar] :elapsed | eta: :eta",
+      total = nrow(tot),
+      width = 60)
+    
+    opts <- list(progress = BEAM_progress)
+    
+    ret <- foreach(i = 1:nrow(tot),
+                   .export = "calc_rmse",
+                   .final = rbindlist,
+                   .packages = c("data.table", "glmmTMB", "ggeffects", "emmeans"),
+                   .options.snow = opts) %dopar% {
+                     calc_rmse(
+                       tot = tot[i],
+                       dat = dat,
+                       analysis_resolution = analysis_resolution,
+                       effort_term = effort_term,
+                       include.weights = include.weights,
+                       weights_values = weights_values
+                     )
+                   }
+    BEAM_pb$terminate()
+    rm("BEAM_pb", envir = .GlobalEnv)
+    return(ret)
+  }
+  
+  
+  #Create new columns for RMSE calculations:
+  
+  ret <- copy(tot)
+  ret[, c("RMSE", "factor", "lower_factor_prop", "upper_factor_prop", "reliability_rmse", "CI_breadth_pass",  "Overall_reliability") :=
+        list(NA_real_, NA_real_, NA_real_, NA_real_, NA, NA, NA)]
+  
+  #Skip cases that do not have a total bycatch estimate or no usable model. Partial estimates will by default have a unreliable estimate. Do we want to change that and still compute RMSE on partial models ?
+  
+  if (is.na(tot$model) || tot$model %in% c("none", "only one") || is.na(tot$tot_mean)) {
+    return(ret)
+  }
+  
+  
+    # helper function (to avoid repeated code). Same as original BEAM, just added weights option
+  fit_and_predict <- function(form, data, levels = NULL) {
+    fit <- suppressMessages(tryCatch({
+      if (isTRUE(include.weights)) {
+        glmmTMB(formula = form, offset = logEffort, family = nbinom2, data = data,weights = weights)
+      } else {
+        glmmTMB(formula = form, offset = logEffort,family = nbinom2,data = data)
+      }
+}, error = function(e) NULL))
+    re <- lme4::findbars(form) # random effects part of model formulation (if any)
+    re <- sapply(re, function(x) as.character(x[[3]]))
+    re.n <- length(re) # number of random effects
+    
+    if (re.n == 0) {
+      pred <- as.data.table(emmeans(fit, ~1, type = "response", offset = log(1)))
+      pred[, predicted := response]
+         } else {
+             type_arg <- ifelse(packageVersion("ggeffects") >= "2.0.0", "random", "re")
+             pred <- ggpredict(model = fit, type =  type_arg, terms = re, verbose = FALSE)
+             pred <- as.data.table(pred)
+             
+             if (!"facet" %in% colnames(pred)) {
+                 pred[, facet := "dummy"]
+             }
+             pred <- pred[order(x, group, facet)]
+         }
+     
+         list(fit = fit, re = re, re.n = re.n, pred = pred)
     }
     
-    # parallelization support
-    if (nrow(bpue) > 1) {
-        cat("calc_rmse: setting up workers")
-        
-        if (exists("BEAM_pb")) {
-            BEAM_pb$terminate()
-            rm("BEAM_pb", envir = .GlobalEnv)
-        }
-        BEAM_pb <<- progress_bar$new(
-            format = "calc_rmse :percent :current/:total [:bar] :elapsed | eta: :eta",
-            total = nrow(bpue),
-            width = 60)
-            
-        opts <- list(progress = BEAM_progress)
-            
-        ret <- foreach(i = 1:nrow(bpue),
-                       .export = "calc_rmse",
-                       .final = unlist,
-                       .packages = c("data.table", "glmmTMB", "ggeffects", "emmeans"),
-                       .options.snow = opts) %dopar% {
-                           calc_rmse(full_data, bpue = bpue[i])
-                       }
-            
-        BEAM_pb$terminate()
-        rm("BEAM_pb", envir = .GlobalEnv)
-        return(ret)
-    }
+    needle <- tot[, ..analysis_resolution]
+    data_subset <- dat[needle, on = analysis_resolution, nomatch =0]
     
-    needle <- bpue[, .(ecoregion = Ecoregion, metierl4 = `metier L4`, species = Species)]
-    data_subset <- full_data[needle, on = c("ecoregion", "metierl4", "species")]
+    #Get column names for later steps, and compute logEffort
+    
+    data_subset[, effort := get(effort_term)]
+    if (isTRUE(include.weights)) data_subset[, weights := get(weights_values)]
+    data_subset <- data_subset[effort > 0]
+    data_subset[, (analysis_resolution) := lapply(.SD, as.factor), .SDcols = analysis_resolution]
+    data_subset[, logEffort := log(effort)]
+    
+    # need at least 2 rows for LOO to be meaningful
+    if (nrow(data_subset) < 2) return(ret)
 
     # Extract the formula of the model
-    formula <- bpue[["BPUE model"]]
-    #formula <- sprintf("%s + offset(logDAS)", formula)
-    form <- as.formula(formula)
+    form <- as.formula(tot$model)
+    if (is.null(form)) return(ret)
     
     # Fit full model
     full_model <- fit_and_predict(form, data_subset)
+    if (is.null(full_model)) return(ret)
 
-    #factor_levels <- list(
-    #    x = levels(full_model$pred$x),
-    #    group = levels(full_model$pred$group),
-    #    facet = levels(full_model$pred$facet)
-    #)
+    #LOO jackknife 
     
     diffs <- sapply(1:nrow(data_subset), function(j) {
-        #jack <- fit_and_predict(form, data = data_subset[-j,], levels = factor_levels)
         jack <- fit_and_predict(form, data = data_subset[-j,])
-        log_diff <- log1p(full_model$pred$predicted) - log1p(jack$pred$predicted)
-        n <- ifelse(jack$re.n == 0, 1, full_model$re.n)
+        log_diff <- log1p(full_model$pred$predicted) - log1p(jack$pred$predicted) #What happens if there are less predictions in the jackknife model ? Because a level of a random effect might have been dropped ? Is that a possibility ?
+        n <- ifelse(jack$re.n == 0, 1, full_model$re.n) #Why are we dividing by number of random effects in the full model ? Can it be a different value that the jackknife model ?
         sum(log_diff^2, na.rm = TRUE) / n
     })
     
-    sqrt(sum(diffs, na.rm=TRUE)/(nrow(data_subset)))
+    #This was happening after calc_rmse in the original function, brought back everything in a single function here.
+    rmse_val <- sqrt(sum(diffs, na.rm=TRUE)/(nrow(data_subset)-1)) #Correction: Added -1 here to match description at top of the function.
+    # Transform back from log scale
+    factor_val <- exp(rmse_val)
+    # The lower limit of the 68% interval is: 100(1-1/factor) # in percentage
+    lower_factor_prop <- 100 * (1 - 1 / factor_val)
+    # The upper limit of the 68% interval is: 100(factor−1) # in percentage
+    upper_factor_prop <- 100 * (factor_val - 1)
+    # RELIABILITY LIMIT ON BOTH CONFIDENCE INTERVALS OF FACTOR
+    # Take the 25% limit of the lower % CI of factor (=80% similarity)
+    # Take the 25% limit of the upper % CIs of factor (=80% similarity)
+    # Which are below 25%
+    
+    ret$tot_lwr_log <- log10(ret$tot_lwr+1)
+    ret$tot_upr_log<-log10(ret$tot_upr+1)
+    ret$CI_breadth<-ret$tot_upr_log-ret$tot_lwr_log
+    
+    ret[, c("RMSE", "factor", "lower_factor_prop", "upper_factor_prop") :=
+          list(rmse_val, factor_val, lower_factor_prop, upper_factor_prop)]
+    
+    #Make all the TRUE/FALSE checks
+    ret[, reliability_rmse        := lower_factor_prop <= 25 & upper_factor_prop <= 25]
+    ret[, CI_breadth_pass := CI_breadth < 2]
+    ret[, Overall_reliability := reliability_rmse & CI_breadth_pass]
+    
+    return(ret)
 }
 
 
-# All species over the new obs3 and bpue_print_table
-rmse <- calc_rmse(full_data = full_data, bpue = bpues_estimates)
-
-# sometimes the above call seems to make the R console unresponsive, even after
-# code execution has completed. I'm not sure why this occurs, and it only happens
-# sometimes. But we can fix it by calling sink() or closeAllConnection()
-# See https://stackoverflow.com/questions/26495498/rstudio-does-not-display-any-output-in-console-after-entering-code
-
-bpues_estimates[, RMSE := rmse]
-
-# Transform back from log scale
-bpues_estimates$factor <- exp(bpues_estimates$RMSE)
-
-# Calculate upper and lower limits
-
-# Estimate delta - breadth of the confidence interval
-
-setnames(bpues_estimates,
-         old = c("TB lower", "TB upper"),
-         new = c("TB.lower", "TB.upper"))                     
-
-bpues_estimates$TB.lower_log <- log10(bpues_estimates$TB.lower+1)
-bpues_estimates$TB.upper_log<-log10(bpues_estimates$TB.upper+1)
-bpues_estimates$delta<-bpues_estimates$TB.upper_log-bpues_estimates$TB.lower_log
 
 
-#Factor=exp(RMSE) # RMSE here is the variable you have in your function but computed with the log modification above
-# The lower limit of the 68% interval is: 100(1-1/factor) # in percentage
-bpues_estimates$lower_factor_prop <- 100*(1-1/bpues_estimates$factor)
-# The upper limit of the 68% interval is: 100(factor−1) # in percentage
-bpues_estimates$upper_factor_prop <- 100*(bpues_estimates$factor-1)
 
-# RELIABILITY LIMIT ON BOTH CONFIDENCE INTERVALS OF FACTOR
-# Take the 25% limit of the lower % CI of factor (=80% similarity)
-# Take the 25% limit of the upper % CIs of factor (=80% similarity)
-# Which are below 25%
-
-# Reliability
-bpues_estimates$reliability <- bpues_estimates$lower_factor_prop <= 25 & bpues_estimates$upper_factor_prop <= 25
-
-# # Plot rmse_log vs delta only for those that we are keeping
-# reliable <- ggplot() +
-  # geom_point(data = new.data[new.data$reliability == T, ], 
-             # aes(RMSE_log, delta), col = "darkgreen") +
-  # theme_bw()
-
-# all_relvalues <- ggplot() +
-  # geom_point(data = new.data, 
-             # aes(RMSE_log, delta), col = "orange") +
-  # theme_bw()
-
-# # Before and after plot
-# ggarrange(all_relvalues, reliable, nrow = 2)
-
-# hist(new.data$delta[new.data$reliability == T], breaks = 200)
-
-# SAVE SOME THINGS
-fwrite(bpues_estimates, "results/bpue_table_print_reliability_subset.csv", na="NA")
 
